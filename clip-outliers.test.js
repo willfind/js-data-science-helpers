0efa3a7a86137f0f16dcf6cb14963cce9748ae51
jrc03c <jrc03c@pm.me>
2021-06-02 12:50:57 -0500

    ...

---
+++ clip-outliers.test.js
@@ -0,0 +1,79 @@
+const clipOutliers = require("./clip-outliers.js")
+
+const {
+  shuffle,
+  sort,
+  random,
+  round,
+  normal,
+  clamp,
+  int,
+} = require("js-math-tools")
+
+test("attempts to clip outliers where there are none", () => {
+  const x = [2, 2, 2, 3, 3, 3, 4, 4, 4]
+  const [yPred, wasTransformed] = clipOutliers(x)
+  expect(yPred).toStrictEqual(x)
+  expect(wasTransformed).toBe(false)
+})
+
+test("attempts to clip outliers where there are none (v2)", () => {
+  const x = clamp(normal(1000), -1, 1)
+  const [yPred, wasTransformed] = clipOutliers(x)
+  expect(yPred).toStrictEqual(x)
+  expect(wasTransformed).toBe(false)
+})
+
+test("attempts to clip outliers on binary data", () => {
+  const x = round(random(1000))
+  const [yPred, wasTransformed] = clipOutliers(x)
+  expect(yPred).toStrictEqual(x)
+  expect(wasTransformed).toBe(false)
+})
+
+test("attempts to clip outliers when there's missing data", () => {
+  const x = [1, 2, 3, "four"]
+  const [yPred, wasTransformed] = clipOutliers(x)
+  expect(yPred).toStrictEqual(x)
+})
+
+test("clips outliers", () => {
+  const x = shuffle([1, 2, 3, 4, 100])
+  const yTrue = [1, 2, 3, 4, 8]
+  const [yPred, wasTransformed] = clipOutliers(x)
+  expect(sort(yPred)).toStrictEqual(yTrue)
+  expect(wasTransformed).toBe(true)
+})
+
+test("clips outliers (v2)", () => {
+  const x = round(random(1000))
+  x[int(random() * x.length)] = -100
+  const [yPred, wasTransformed] = clipOutliers(x)
+  expect(wasTransformed).toBe(true)
+})
+
+test("throws an error when attempting to clip outliers on non-vectors of non-numbers", () => {
+  expect(() => {
+    clipOutliers()
+  }).toThrow()
+
+  expect(() => {
+    clipOutliers(normal([5, 5, 5, 5]))
+  }).toThrow()
+
+  expect(() => {
+    clipOutliers([1, 2, 3], "four")
+  }).toThrow()
+
+  expect(() => {
+    clipOutliers(true, false)
+  }).toThrow()
+
+  expect(() => {
+    clipOutliers(null, undefined)
+  }).toThrow()
+
+  expect(() => {
+    clipOutliers(() => {}, {})
+  }).toThrow()
+})
